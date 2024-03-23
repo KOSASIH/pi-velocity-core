de85f3bede8048d38febcc3adfce9ac98fb35e38
KOSASIH <kosasihg88@gmail.com>
2024-03-23 21:32:00 +0700

    Create __init__.py

---
+++ test/__init__.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import unittest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import models
+import views
+
+from unittest import TestCase
