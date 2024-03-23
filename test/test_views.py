941e3a1befa06c2a71a83380c3acf2c64711e4ac
KOSASIH <kosasihg88@gmail.com>
2024-03-23 21:34:23 +0700

    Create test_views.py

---
+++ test/test_views.py
@@ -0,0 +1,72 @@
+import json
+
+import unittest
+
+import app
+
+from test.__init__ import TestCase
+
+class TestViews(TestCase):
+
+    def test_index(self):
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_blocks(self):
+        response = self.client.get('/blocks')
+
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.data)
+
+        self.assertGreaterEqual(len(data), 1)
+
+    def test_block(self):
+        block = models.Block.create(data='Test Block')
+
+        response = self.client.get(f'/blocks/{block.id}')
+
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.data)
+
+        self.assertEqual(data['data'], 'Test Block')
+
+    def test_create_block(self):
+        data = {'data': 'Test Block'}
+
+        response = self.client.post('/blocks', json=data)
+
+        self.assertEqual(response.status_code, 201)
+
+        data = json.loads(response.data)
+
+        self.assertEqual(data['data'], 'Test Block')
+
+    def test_update_block(self):
+        block = models.Block.create(data='Test Block')
+
+        data = {'data': 'New Test Block'}
+
+        response = self.client.put(f'/blocks/{block.id}', json=data)
+
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.data)
+
+        self.assertEqual(data['data'], 'New Test Block')
+
+    def test_delete_block(self):
+        block = models.Block.create(data='Test Block')
+
+        response = self.client.delete(f'/blocks/{block.id}')
+
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(f'/blocks/{block.id}')
+
+        self.assertEqual(response.status_code, 404)
+
+if __name__ == '__main__':
+    unittest.main()
