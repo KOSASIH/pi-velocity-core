b831e4356504ee7dad9e32addee29daa4d6f9be9
KOSASIH <kosasihg88@gmail.com>
2024-03-23 21:57:31 +0700

    Create run.sh

---
+++ web/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the web directory in the pi-velocity-core git repository
+cd /path/to/pi-velocity-core/web
+
+# Perform the desired operation (e.g., start a server, run tests, build documentation, etc.)
+npm start
