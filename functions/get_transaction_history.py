9a3b38327f59e6bdb657a3b669aa39155d75c99c
KOSASIH <kosasihg88@gmail.com>
2024-03-25 00:16:24 +0700

    Create get_transaction_history.py

---
+++ functions/get_transaction_history.py
@@ -0,0 +1,19 @@
+def get_transaction_history(user_address, pi_network):
+    """
+    Retrieve the transaction history of a user.
+
+    :param user_address: The address of the user.
+    :param pi_network: The Pi network object.
+    :return: A list of transactions.
+    """
+
+    # Get all transactions associated with the user's address
+    user_transactions = pi_network.get_transactions_by_address(user_address)
+
+    # Filter out the incoming and outgoing transactions
+    transaction_history = [
+        t for t in user_transactions
+        if t['from'] == user_address or t['to'] == user_address
+    ]
+
+    return transaction_history
