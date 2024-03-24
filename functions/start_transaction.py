1c2a2e31aa01bc5ea7595e6b8d7e9269cd6c97ef
KOSASIH <kosasihg88@gmail.com>
2024-03-24 23:58:55 +0700

    Create start_transaction.py

---
+++ functions/start_transaction.py
@@ -0,0 +1,14 @@
+def start_transaction():
+    """
+    Initiates a new transaction.
+    """
+    # Create a new transaction object
+    transaction = {
+        'id': str(uuid.uuid4()),
+        'sender': None,
+        'receiver': None,
+        'amount': None,
+        'fee': None,
+        'timestamp': datetime.datetime.now()
+    }
+    return transaction
